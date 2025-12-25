dafce4dd91600fad2678f629e7eb915d4035b77d
324094432 <guy.kaizer@gmail.com>
2025-12-25 17:54:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-mport pandas as pd
+import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
